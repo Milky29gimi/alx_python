5ad63c7e46b443f90e2e1493630d917c8ec209e5
milky kerenso <milkykerenso@gmail.com>
2023-12-07 11:51:21 +0300

    add commit 4

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,10 @@
 #0. Import a simple function from a simple file
+
 def add(a,b):
     return (a+b)
 
 a=1
 b=2
 
-if [__name__]==["__main__"]
-from add_0 import add
-print("{}+{}".format(a,b))
-1+2=3
-add= ["a"+"b"] 
-print(add)
+[__name__]==["__main__"]
+print ("{}+{}={}".format(a,b,add(a,b)))
