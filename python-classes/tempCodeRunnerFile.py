f7c63ec458b4874e97eea067b585e36b42b78ebd
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 15:06:26 +0300

    commit 13

--- python-classes/tempCodeRunnerFile.py
+++ python-classes/tempCodeRunnerFile.py
@@ -1,11 +1,11 @@
- # def get_size(self):
-    #     return self.__size
+    def get_size(self):
+        return self.__size
 
-    # def set_size(self, new_size):
-    #     self.__size = new_size
+    def set_size(self, new_size):
+        self.__size = new_size
 
-    # def area(self):
-    #     return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
 
-    # def perimeter(self):
-    #     return 4 * self.__size
+    def perimeter(self):
+        return 4 * self.__size
