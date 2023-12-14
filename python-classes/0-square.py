6635a71a0cf429509d4588a1efd18be88435d49e
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 18:02:19 +0300

    connit 26

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,10 @@
 """ create a class called square
-and define a private instance attribute calles size. """
+and define a private instance attribute calles size.  """
 
 class Square:
    
     def __init__(self, size):
-        
+        """
+        @__size:is a private instance attribut
+        """
         self.__size = size
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,9 @@ Size validation- input validater"""
 class Square:
     __size= ()
     def __init__(self, size=0):
-    
+    """ create a class called square
+and define a private instance attribute calles size. 
+Size validation- input validater"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
