d87a978132c96f89ad2160fb42775d6b5e5945a8
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 18:12:14 +0300

    commit 30

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,10 +3,14 @@ and define a private instance attribute calles size.
 Size validation- input validater"""
 
 class Square:
+
       """This id a Square class."""
+
     __size= ()
     def __init__(self, size=0):
-        """ Size validation- input validater  """
+
+        """ @__size:is a private instance attribut
+        Size validation- input validater  """
         
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
