b68aeee9a8af1524343f6089e479f5274a792169
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 15:49:59 +0300

    commit 19

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,16 +1,16 @@
 
-from 0-square import Square
+# from 0-square import Square
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+# my_square = Square(3)
+# print(type(my_square))
+# print(my_square.__dict__)
 
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
+# try:
+#     print(my_square.size)
+# except Exception as e:
+#     print(e)
 
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+# try:
+#     print(my_square.__size)
+# except Exception as e:
+#     print(e)
