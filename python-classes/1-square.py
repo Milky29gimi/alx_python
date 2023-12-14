6df371be8cfe0a3a2a9e44e34f5b4b0e7711d321
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 18:13:24 +0300

    commit 31

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -14,7 +14,11 @@ class Square:
         
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+            """ @__size:is a private instance attribut
+        Size validation- input validater  """
         elif size < 0:
             raise ValueError("size must be >= 0")
+            """ @__size:is a private instance attribut
+        Size validation- input validater  """
         self.__size = size
     
