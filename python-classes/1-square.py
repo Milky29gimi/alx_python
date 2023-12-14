238eb7011e394ee20d0fdb9e6e22d7968ac5f103
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 16:09:03 +0300

    commit 20

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,4 +4,9 @@ class Square:
     __size= ()
     def __init__(self, size=0):
     
-size = Square()
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
