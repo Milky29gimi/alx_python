4bc11c8af1154ecfba2c0c197b0b2bbdc9e9a8ff
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 23:04:05 +0300

    commit 40

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,32 +4,37 @@ Size validation- input validater
 the area () method claculates the area of the square by multipling."""
 
 class Square:
-      """
-    This id a Square class.
+       """A class that defines a square.
+
+    Attributes:
+        __size (int): Represents the size of the square.
     """
+
     def __init__(self, size=0):
-        """ @__size:is a private instance attribut
-        Size validation- input validater  """
+          """Initializes a Square instance with an optional size parameter.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If the size parameter is not an integer.
+            ValueError: If the size parameter is less than 0.
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-            """ @__size:is a private instance attribut
-        Size validation- input validater  """
-
         elif size < 0:
             raise ValueError("size must be >= 0")
-
-            """ @__size:is a private instance attribut
-        Size validation- input validater  """
 
         self.__size = size
        
 
 
     def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
-        
-        """ @__size:is a private instance attribut
-        Size validation- input validater  """
-    
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,44 +1,58 @@
-""" create a class called square
-and define a private instance attribute calles size. 
-Size validation- input validater"""
+"""Access and update private attribute"""
 
 class Square:
-      """
-    This id a Square class.
+   """A class that represents a square.
+
+    Attributes:
+        __size (int): Represents the size of the square.
     """
+
     def __init__(self, size=0):
-        """ @__size:is a private instance attribut
-        Size validation- input validater  """
+
+         """Initializes a Square instance with an optional size parameter.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If the size parameter is not an integer.
+            ValueError: If the size parameter is less than 0.
+        """
 
         self.size = size
 
 
     @property
     def size(self):
+        """Getter method to retrieve the size of the square."""
         return self.__size
-        """ @__size:is a private instance attribut
-        Size validation- input validater  """
-
+      
     @size.setter
     def size(self, value):
-        """ @__size:is a private instance attribut
-        Size validation- input validater  """
+       """Setter method to set the size of the square.
+
+        Args:
+            value (int): The size to set for the square.
+
+        Raises:
+            TypeError: If the value parameter is not an integer.
+            ValueError: If the value parameter is less than 0.
+        """
 
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-            
-            """ @__size:is a private instance attribut
-        Size validation- input validater  """
-
+            raise TypeError("size must be an integer") 
+         
         elif value < 0:
             raise ValueError("size must be >= 0")
-
-            """ @__size:is a private instance attribut
-        Size validation- input validater  """
 
         self.__size = value
 
     def area(self):
+         """Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
         """ @__size:is a private instance attribut
         Size validation- input validater  """
