d7571a64ae307c8cbbff9e62840cc2b4b91c917a
Milky Kerenso <milkykerenso@gmail.com>
2023-12-14 15:45:27 +0300

    commit 18

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,32 +9,3 @@ class Square:
         @__size:is a private instance varible
         """
         self.__size = size
-
-    # def get_size(self):
-    #     return self.__size
-
-    # def set_size(self, new_size):
-    #     self.__size = new_size
-
-    # def area(self):
-    #     return self.__size ** 2
-
-    # def perimeter(self):
-    #     return 4 * self.__size
-
-
-if __name__ == "__main__":
-    my_square = Square(89)
-    print(type(my_square))
-    print(my_square.__dict__)
-
-    try:
-        print(my_square.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square.__size)
-    except Exception as e:
-        print(e)
-
