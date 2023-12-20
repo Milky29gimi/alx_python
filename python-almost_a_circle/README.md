c2ea793864cbdd3b1a6f085a4e4bd951dec375f8
Milky Kerenso <milkykerenso@gmail.com>
2023-12-20 16:05:23 +0300

    commit 1 task 1

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,4 @@
+python task week 4 (last week)
+task 1
+
+python-almost_a_circle
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+import os    
+    # Create the models package if it doesn't exist
+if not os.path.exists("models"):
+    os.mkdir("models")
+    with open("models/__init__.py", "w"):
+        pass
+
+class Base:
+    # Private class attribute
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            # Increment __nb_objects and assign the new value to the public instance attribute id
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
