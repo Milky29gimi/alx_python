694d72a88ab2b0ad92c27dad67b505d5a88a7af6
Milky Kerenso <milkykerenso@gmail.com>
2023-12-20 17:23:44 +0300

    commit 3

---
+++ python-almost_a_circle/0-hbtn_status.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Status code: 200 OK")
+    print("Response body:")
+    for line in response.text.splitlines():
+        print(f"\t- {line}")
+else:
+    print(f"Request failed with status code {response.status_code}")
