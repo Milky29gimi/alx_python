ccde5d5809c01b0d8cc4f477798769487b619e99
Milky Kerenso <milkykerenso@gmail.com>
2023-12-20 17:46:35 +0300

    commit 6

---
+++ python-almost_a_circle/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+url = 'https://alu-intranet.hbtn.io/status'
+# response = requests.get(url)
+
+# if response.status_code == 200:
+#     print("Status code: 200 OK")
+#     print("Response body:")
+#     for line in response.text.splitlines():
+#         print(f"\t- {line}")
+# else:
+#     print(f"Request failed with status code {response.status_code}")
