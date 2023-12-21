f9db066db9eef545be3acb0b0895a5f8224538c2
Milky Kerenso <milkykerenso@gmail.com>
2023-12-21 19:54:06 +0300

    commit 24

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
-"""Python script that fetches https://alu-intranet.hbtn.io/status"""
 import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 
 if response.status_code == 200:
-    print("Body response:")
-    print("type: <class 'str'>")
-    
-    for line in response.text.splitlines():
-        print(f"\t- {line}")
+    data = response.json()
+    for key, value in data.items():
+        print(f"- {key}: {value}")
 else:
-    print(f"Request failed with status code {response.status_code}")
-    
+    print(f"Error: {response.status_code}")
