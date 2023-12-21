7deb4f3795f4eedffeb1386cb4254232d844a282
Milky Kerenso <milkykerenso@gmail.com>
2023-12-21 18:35:59 +0300

    commit 20

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,12 +1,23 @@
-"""create a class named Squarethat inherits from Rectangle """
+"""create a class named Square that inherits from Rectangle """
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ define the class Square"""
     def __init__(self, size, x=0, y=0, id=None):
-         """ define the class Square"""
+        """ define the class Square"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ define the class Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ define the class Square"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
-         """ define the class Square"""
+        """ define the class Square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
