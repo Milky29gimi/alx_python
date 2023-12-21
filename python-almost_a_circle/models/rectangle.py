672989939377da44b41e497ee08ad2505095c80b
Milky Kerenso <milkykerenso@gmail.com>
2023-12-21 17:49:30 +0300

    commit 17

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,16 @@
 """create a class named Rectangle that inherits from Base """
 from models.base import Base
 
+
 class Rectangle(Base):
-    """ define the class Rectangle"""
+     """ define the class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """ define the class Rectangle"""
         super().__init__(id)
+        self.__width = None
+        self.__height = None
+        self.__x = None
+        self.__y = None
         self.width = width
         self.height = height
         self.x = x
@@ -66,3 +71,44 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0.")
         self.__y = value
+
+    def area(self):
+        """ define the class Rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """ define the class Rectangle"""
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def update(self, *args, **kwargs):
+        """ define the class Rectangle"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'width':
+                    self.width = value
+                elif key == 'height':
+                    self.height = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
+
+    def __str__(self):
+        """ define the class Rectangle"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
