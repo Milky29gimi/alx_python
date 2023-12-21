4dd69a52e78ed41068474293d9e74d1a81cdc22f
Milky Kerenso <milkykerenso@gmail.com>
2023-12-21 18:49:45 +0300

    commit 22

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,9 +5,6 @@ class Square(Rectangle):
     """ define the class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """ define the class Square"""
-        self.__width = 0
-        self.__height = 0
-        self.__size = size
         super().__init__(size, size, x, y, id)
 
     @property
