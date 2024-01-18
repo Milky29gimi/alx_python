6272f7e48262bafc188d3440850f7c9a0e0b3d6b
Milky Kerenso <milkykerenso@gmail.com>
2024-01-18 14:23:01 +0300

    commit 14

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-# A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+# A  script that lists all states from the database hbtn_0e_0_usa:
 import mysql.connector
 
 # Connect to the database
