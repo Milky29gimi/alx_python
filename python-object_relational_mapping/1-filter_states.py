1bf762806ed3fe4beeb9d4f2f1b8f161499b1636
Milky Kerenso <milkykerenso@gmail.com>
2024-01-18 15:09:43 +0300

    commit 24

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ password = sys.argv[2]
 database = sys.argv[3]
 
 # Connect to the MySQL server
-db = MySQLdb.connect(host='localhost', port=3306, user=milky kerenso, passwd=Abdi29gimi!, db=hbtn_0e_0_usa)
+db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
 
 # Create a cursor object to execute SQL queries
 cursor = db.cursor()
