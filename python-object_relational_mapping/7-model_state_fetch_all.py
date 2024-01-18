e5b518e15f13088874826fedb513535058b8bee8
Milky Kerenso <milkykerenso@gmail.com>
2024-01-18 21:54:15 +0300

    commit aabbdd

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,16 @@
-# All states via SQLAlchemy
+'''All states via SQLAlchemy'''
 
-import sys
+from model_state import Base, State
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if name == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py [mysql username] [mysql password] [database name]")
-        sys.exit(1)
+path = "mysql+mysqldb://{}:{}@localhost/{}"
+engine = create_engine(path.format(argv[1], argv[2], argv[3]))
 
-    # Get MySQL credentials from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+Session = sessionmaker(bind = engine)
+session = Session()
 
-    # Create engine and session
-    engine = create_engine(f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query all states and sort by states.id in ascending order
-    states = session.query(State).order_by(State.id).all()
-
-    # Display the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
-    session.close()
+for states in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(states.id, states.name))
+session.close()
