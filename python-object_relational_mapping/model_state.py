00a99458bb30132cd1bb33a90371484916182758
Milky Kerenso <milkykerenso@gmail.com>
2024-01-18 16:16:01 +0300

    commit 62

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,12 @@
 # A python file that contains the class definition of a State and an instance Base = declarative_base():
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-# Define the declarative base
 Base = declarative_base()
 
-# Define the State class
+
 class State(Base):
     tablename = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-# Connect to the MySQL server
-engine = create_engine('mysql://<username>:<password>@localhost:3306/<database>')
-
-# Create all tables
-Base.metadata.create_all(engine)
