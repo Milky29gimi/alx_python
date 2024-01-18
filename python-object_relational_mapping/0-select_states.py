e4b61dc436881d01bcb8c8959f2643968625d916
Milky Kerenso <milkykerenso@gmail.com>
2024-01-18 13:34:04 +0300

    commit 12

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
+
+def list_states(username, password, database):
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve states sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
 
 if name == "__main__":
     # Check if all three arguments are provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Extracting command line arguments
-    username, password, database = argv[1], argv[2], argv[3]
+    # Get MySQL credentials from command line arguments
+    username, password, database = sys.argv[1:4]
 
-    # Connect to MySQL server
-    try:
-        connection = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = connection.cursor()
-
-        # Execute query
-        query = "SELECT * FROM states ORDER BY states.id ASC"
-        cursor.execute(query)
-
-        # Fetch and display results
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error {}: {}".format(e.args[0], e.args[1]))
-    finally:
-        # Close the connection
-        if 'connection' in locals():
-            connection.close()
+    # Call the function to list states
+    list_states(username, password, database)
