8e0ac5926c5bdc84292ae693e2be631e890f3361
Milky Kerenso <milkykerenso@gmail.com>
2024-01-18 21:04:04 +0300

    commit 2x

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,30 +1,14 @@
 'A python file that contains the class definition of a State and an instance Base = declarative_base():'
    
-    'import Column, Integer, String'
+''' importing sqlalchemy'''
 from sqlalchemy import Column, Integer, String
-
-   ' import declarative_base '
-    
 from sqlalchemy.ext.declarative import declarative_base
 
-    ' Base = declarative_base '
-    
 Base = declarative_base()
 
- 
-    ' Represents a state in the states table.'
-    
 class State(Base):
-    
-    ' Represents a state in the states table. '
-    
-
-    tablename = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    'Unique identifier for the state. Type: Integer Constraints: Primary key, not null.'
-  
-
+    ''' class State that inherits from Base '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    'Name of the state. Type: String Constraints: Maximum length of 128 characters, not null.'
  
