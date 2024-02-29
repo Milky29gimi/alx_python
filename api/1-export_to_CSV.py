dfe6224fc97714cccff6f885119aa0a5759871e5
Milky Kerenso <milkykerenso@gmail.com>
2024-02-29 09:36:27 +0300

    commit 5

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,27 @@
 import csv
 import requests
-import sys
 
-def get_employee_info(employee_id):
-    # Get employee details
-    employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    employee_data = employee_response.json()
-    employee_name = employee_data['username']
+def export_todo_all_employees():
+    # Get list of users
+    users_response = requests.get('https://jsonplaceholder.typicode.com/users')
+    users_data = users_response.json()
 
-    # Get TODO list for the employee
-    todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
-    todo_data = todo_response.json()
+    for user in users_data:
+        user_id = user['id']
+        username = user['username']
 
-    # Write data to CSV file
-    filename = f"{employee_id}.csv"
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todo_data:
-            writer.writerow([employee_id, employee_name, task['completed'], task['title']])
-    print(f"CSV file '{filename}' has been created successfully.")
+        # Get TODO list for the user
+        todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
+        todo_data = todo_response.json()
+
+        # Write data to CSV file
+        filename = f"{user_id}.csv"
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            for task in todo_data:
+                writer.writerow([user_id, username, task['completed'], task['title']])
+        print(f"CSV file '{filename}' has been created successfully.")
 
 if name == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+    export_todo_all_employees()
